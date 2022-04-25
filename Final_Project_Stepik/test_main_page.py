9163e72176975ddbbcf9caeb594a7bf222ba2338
Orkhan Ali <56834314+apollon-galaks@users.noreply.github.com>
2022-04-25 14:06:00 +0300

    Update test_main_page.py

--- Final_Project_Stepik/test_main_page.py
+++ Final_Project_Stepik/test_main_page.py
@@ -5,7 +5,8 @@ from .pages.locators import BasePageLocators
 import pytest
 
 
-@pytest.mark.skip
+
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = MainPage(browser,url)
@@ -13,14 +14,13 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.solve_quiz_and_get_code()
     page.is_not_element_present(*MainPageLocator.SUCCESS_MESSAGE)
 
-@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = MainPage(browser,url)
     page.open()
     page.is_not_element_present(*MainPageLocator.SUCCESS_MESSAGE)
 
-@pytest.mark.skip
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = MainPage(browser,url)
@@ -28,11 +28,23 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.solve_quiz_and_get_code()
     page.is_disappeared(*MainPageLocator.SUCCESS_MESSAGE)
     
-@pytest.mark.skip
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
     def test_guest_should_see_login_link_on_product_page(self, browser):
         url = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = MainPage(browser,url)
+        page.open()
+        page.should_be_login_link()
+
+    @pytest.mark.xfail
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        url = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = MainPage(browser,url)
+        page.open()
+        page.go_to_login_page()
+    
+
+
         page = MainPage(browser,url)
         page.open()
         page.should_be_login_link()
