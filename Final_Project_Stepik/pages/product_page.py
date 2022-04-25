771911cd8dee81116c665a4766daf75d9fd56f52
Orkhan Ali <56834314+apollon-galaks@users.noreply.github.com>
2022-04-25 13:37:16 +0300

    Update product_page.py

--- Final_Project_Stepik/pages/product_page.py
+++ Final_Project_Stepik/pages/product_page.py
@@ -1,10 +1,6 @@
 from .base_page import BasePage
-from .locators import ProductPageLocators
 from selenium.webdriver.common.by import By
 
 class ProductPage(BasePage):
-
-
-    def add_to_cart(self):
-        self.browser.find_element(*ProductPageLocators.ADD_TO_CART)
-        #add_cart.click()
+    def __init__(self, *args, **kwargs):
+        super(ProductPage, self).__init__(*args, **kwargs)
