2b5b7e5364fd519a2cbeadd5bb906da121cd5ef1
Orkhan Ali <56834314+apollon-galaks@users.noreply.github.com>
2022-04-25 13:12:05 +0300

    Update test_product_page.py

--- Final_Project_Stepik/test_product_page.py
+++ Final_Project_Stepik/test_product_page.py
@@ -1,7 +1,33 @@
+from .pages.base_page import BasePage
 from .pages.product_page import ProductPage
+from .pages.locators import ProductPageLocators
+from.pages.locators import MainPageLocator
+from .pages.login_page import LoginPage
+import time
 
-def test_guest_can_add_product_to_basket(browser):
-    url = '(http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    page = ProductPage(browser, url)
-    page.open()
-    page.add_to_cart()
+import pytest
+
+
+class TestGuestAddToBasketFromProductPage():
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = page = LoginPage(browser, link)
+        page.open()
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time())
+        page.register_new_user(email=email, password=password)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser,url)
+        page.open()
+        page.is_not_element_present(*ProductPageLocators.ADD_TO_CART)
+    
+    def test_guest_can_add_product_to_basket(self, browser):
+        url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ProductPage(browser, url)
+        page.open()
+        page.add_to_cart(*MainPageLocator.LOGIN_LINK)
