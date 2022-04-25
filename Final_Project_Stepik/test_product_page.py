1660a02c4a463fd6d85930fd673eafd996b75712
Orkhan Ali <56834314+apollon-galaks@users.noreply.github.com>
2022-04-25 14:07:27 +0300

    Update test_product_page.py

--- Final_Project_Stepik/test_product_page.py
+++ Final_Project_Stepik/test_product_page.py
@@ -7,8 +7,31 @@ import time
 
 import pytest
 
-
+@pytest.mark.need_review
 class TestGuestAddToBasketFromProductPage():
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser,url)
+        page.open()
+        page.is_not_element_present(*ProductPageLocators.LOGIN_FROM_PP)
+
+
+    def test_guest_cant_see_success_message(self, browser):
+        url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser,url)
+        page.open()
+        page.is_not_element_present(*ProductPageLocators.ADD_TO_CART)
+        
+    def test_guest_can_add_product_to_basket(self, browser):
+        url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ProductPage(browser, url)
+        page.open()
+        page.add_to_cart(*MainPageLocator.LOGIN_LINK)
+
+
+
+@pytest.mark.need_review
+class TestUserAddToBasketFromProductPage():
 
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
@@ -26,7 +49,7 @@ class TestGuestAddToBasketFromProductPage():
         page.open()
         page.is_not_element_present(*ProductPageLocators.ADD_TO_CART)
     
-    def test_guest_can_add_product_to_basket(self, browser):
+    def test_user_can_add_product_to_basket(self, browser):
         url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         page = ProductPage(browser, url)
         page.open()
